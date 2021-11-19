7876ed2328a7177cc71b62d3223e64ed75a647d9
poliweb <web@poliweb.su>
2021-11-19 13:52:22 +0600

    Update

--- pages/models.vue
+++ pages/models.vue
@@ -6,7 +6,7 @@
       <div class="masck__header-page"></div>
       <div class="container mx-auto py-28 flex flex-col place-content-center text-center ">
         <h2 class="title-dark text-5xl md:text-7xl font-bold z-10">Our Models</h2>
-        <p class="text-white z-10">The only Mastermind course you will ever need for winning big on Crypto markets and trading.</p>
+        <p class="text-white leading-relaxed text-2xl px-5 z-10">The only Mastermind course you will ever need for winning big on Crypto markets and trading.</p>
       </div>
     </div>
     <!-- Content page -->
