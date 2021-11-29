80fcf36ccc3bb087a16d5e6fcde2cba6a65386cf
poliweb <web@poliweb.su>
2021-11-29 14:06:57 +0600

    Add pages/about.vue Create "Section Our Story +  Header page" and "Section ModelAgency-Fotoalbom-Fancybox"

---
+++ pages/about.vue
@@ -0,0 +1,171 @@
+<template>
+  <div>
+    <!-- Section Our Story +  Header page -->
+    <section class="body-font">
+      <!-- Header page -->
+      <div class="relative">
+        <img
+          src="https://res.cloudinary.com/poliweb/image/upload/c_crop,g_face:center,h_400,w_1920/v1636955920/HeroCarousel/slide-girl-face2_lwgpha.webp"
+          alt="About our Agency"
+          class="absolute h-full w-full object-cover z-0"
+        />
+        <div class="masck__header-page"></div>
+        <div class="container mx-auto py-28 flex flex-col place-content-center text-center">
+          <h2 class="title-dark text-5xl md:text-7xl font-bold z-10">About our Agency</h2>
+          <p
+            class="text-white leading-relaxed text-2xl px-5 z-10"
+          >Learn More about our model agency.</p>
+        </div>
+      </div>
+      <!-- //Header page -->
+      <!-- Our Story -->
+      <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center max-w-6xl">
+        <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+          <div class="overflow-hidden model-img">
+            <a
+              data-fancybox="single"
+              data-src="https://res.cloudinary.com/poliweb/image/upload/c_fill,g_center,h_1283/v1638168439/Fashen/FashionWeek/fashion_week_1_wrvc6f.webp"
+            >
+              <img
+                class="object-cover object-center"
+                alt="hero"
+                src="https://res.cloudinary.com/poliweb/image/upload/c_fill,g_center,h_600/v1638168439/Fashen/FashionWeek/fashion_week_1_wrvc6f.webp"
+              />
+            </a>
+          </div>
+        </div>
+        <div
+          class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center"
+        >
+          <h3 class="uppercase">How we started</h3>
+          <h2
+            class="title-color title-font text-3xl md:text-4xl title-font font-semibold mb-4"
+          >Our Story</h2>
+          <p
+            class="mb-2 leading-relaxed"
+          >Cloud bread flannel poke, flexitarian vinyl iPhone church-key shaman williamsburg kitsch beard.</p>
+          <p
+            class="mb-2 leading-relaxed"
+          >Rapidiously supply superior e-commerce with extensible technologies. Competently predominate excellent convergence vis-a-vis optimal sources. Conveniently coordinate multimedia based innovation.</p>
+          <p
+            class="mb-2 leading-relaxed"
+          >Energistically embrace intermandated resources before wireless testing procedures. Authoritatively reinvent plug-and-play expertise via high-quality sources. Appropriately drive professional manufactured.</p>
+        </div>
+      </div>
+      <!-- //Our Story -->
+    </section>
+    <!-- //Section Our Story +  Header page -->
+    <!-- Section ModelAgency-Fotoalbom-Fancybox -->
+    <section class="body-font">
+      <div class="px-0 md:px-5 py-24 mx-auto max-w-6xl">
+        <div class="flex flex-wrap gap-5 justify-center">
+          <div class="overflow-hidden model-img">
+            <a data-fancybox-trigger="gallery-b" href="javascript:;">
+              <img
+                class="object-cover object-center"
+                src="https://res.cloudinary.com/poliweb/image/upload/c_fill,g_center,h_600,w_1152/v1638168437/Fashen/FashionWeek/fashion_week_7_wl0su7.webp"
+              />
+            </a>
+          </div>
+          <div class="overflow-hidden model-img rounded">
+            <a
+              data-fancybox="gallery-b"
+              href="https://res.cloudinary.com/poliweb/image/upload/c_fill,g_center,h_1279,w_1920/v1638168437/Fashen/FashionWeek/fashion_week_7_wl0su7.webp"
+            >
+              <img
+                class="rounded"
+                src="https://res.cloudinary.com/poliweb/image/upload/c_fill,g_center,h_80,w_120/v1638168437/Fashen/FashionWeek/fashion_week_7_wl0su7.webp"
+              />
+            </a>
+          </div>
+          <div class="overflow-hidden model-img rounded">
+            <a
+              data-fancybox="gallery-b"
+              href="https://res.cloudinary.com/poliweb/image/upload/c_fill,g_center,h_1281,w_1920/v1638168439/Fashen/FashionWeek/fashion_week_2_sfmgpx.webp"
+            >
+              <img
+                class="rounded"
+                src="https://res.cloudinary.com/poliweb/image/upload/c_fill,g_center,h_80/v1638168439/Fashen/FashionWeek/fashion_week_2_sfmgpx.webp"
+              />
+            </a>
+          </div>
+          <div class="overflow-hidden model-img rounded">
+            <a
+              data-fancybox="gallery-b"
+              href="https://res.cloudinary.com/poliweb/image/upload/c_fill,g_center,h_1281,w_1920/v1638168437/Fashen/FashionWeek/fashion_week_3_jssinu.webp"
+            >
+              <img
+                class="rounded"
+                src="https://res.cloudinary.com/poliweb/image/upload/c_fill,g_center,h_80,w_120/v1638168437/Fashen/FashionWeek/fashion_week_3_jssinu.webp"
+              />
+            </a>
+          </div>
+          <div class="overflow-hidden model-img rounded">
+            <a
+              data-fancybox="gallery-b"
+              href="https://res.cloudinary.com/poliweb/image/upload/c_fill,g_center,h_1282,w_1920/v1638168437/Fashen/FashionWeek/fashion_week_4_cfdjwj.webp"
+            >
+              <img
+                class="rounded"
+                src="https://res.cloudinary.com/poliweb/image/upload/c_fill,g_center,h_80,w_120/v1638168437/Fashen/FashionWeek/fashion_week_4_cfdjwj.webp"
+              />
+            </a>
+          </div>
+          <div class="overflow-hidden model-img rounded">
+            <a
+              data-fancybox="gallery-b"
+              href="https://res.cloudinary.com/poliweb/image/upload/c_fill,g_center,h_1280,w_1920/v1638168438/Fashen/FashionWeek/fashion_week_5_cjgnns.webp"
+            >
+              <img
+                class="rounded"
+                src="https://res.cloudinary.com/poliweb/image/upload/c_fill,g_center,h_80,w_120/v1638168438/Fashen/FashionWeek/fashion_week_5_cjgnns.webp"
+              />
+            </a>
+          </div>
+          <div class="overflow-hidden model-img rounded">
+            <a
+              data-fancybox="gallery-b"
+              href="https://res.cloudinary.com/poliweb/image/upload/c_fill,g_center,h_1283,w_1920/v1638168437/Fashen/FashionWeek/fashion_week_6_prlbiy.webp"
+            >
+              <img
+                class="rounded"
+                src="https://res.cloudinary.com/poliweb/image/upload/c_fill,g_center,h_80,w_120/v1638168437/Fashen/FashionWeek/fashion_week_6_prlbiy.webp"
+              />
+            </a>
+          </div>
+          <div class="overflow-hidden model-img rounded">
+            <a
+              data-fancybox="gallery-b"
+              href="https://res.cloudinary.com/poliweb/image/upload/c_fill,g_center,h_1281,w_1920/v1638168438/Fashen/FashionWeek/fashion_week_8_fpfuy1.webp"
+            >
+              <img
+                class="rounded"
+                src="https://res.cloudinary.com/poliweb/image/upload/c_fill,g_center,h_80,w_120/v1638168438/Fashen/FashionWeek/fashion_week_8_fpfuy1.webp"
+              />
+            </a>
+          </div>
+          <div class="overflow-hidden model-img rounded">
+            <a
+              data-fancybox="gallery-b"
+              href="https://res.cloudinary.com/poliweb/image/upload/c_fill,g_center,h_1280,w_1920/v1636096462/Fashen/catwalk_1920_dgoled.webp"
+            >
+              <img
+                class="rounded"
+                src="https://res.cloudinary.com/poliweb/image/upload/c_fill,g_center,h_80,w_120/v1636096462/Fashen/catwalk_1920_dgoled.webp"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- //Section ModelAgency-Fotoalbom-Fancybox -->
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
